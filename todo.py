dc58e104136b15d6eeb4bf241cb453d64803d4e2
JoeBum <66751176+JoeBumm@users.noreply.github.com>
2024-03-17 01:35:51 +0200

    Added a feature to view your tasks and to delete them

--- todo.py
+++ todo.py
@@ -9,25 +9,39 @@ def task_handler(task):
     task_list = [task] + task_list
     return task_list 
 
-def store_txt(txt):
+def store_txt(txt,nav):
     #this function has two modes, write and append
-    with open('task.txt' , 'a') as file:
-        file.write(format_text(task))
-        file.write('\n')
+    if nav==1:
+        with open('task.txt' , 'a') as file:
+            file.write(format_text(task))
+            file.write('\n')
+#This checks whether the user wants to read the text file
+    if nav==2:
+        f = open('task.txt', 'r')
+        file_contents = f.read()
+        print (file_contents)
+    if nav==9:
+        with open('task.txt', 'w') as file:
+            file.write("")
 
 #user prompt
 print("""Please Choose your service
       Press 1 for adding todolists, add each one per time
       press 2 for viewing your todo list (coming soon)
-      press -1 for ending the process""")
+      press -1 for ending the process
+      For deleting all your tasks, press 9 (no undo !!)""")
 
+# tha navigation toolbox for the user
 navigation = int(input("Enter the chosen service: "))
+
+
 while navigation!=-1 and navigation==1:
     task = input("What do you want to accomplish today? ")
-    store_txt(task)
+    store_txt(task,1)
     navigation = int(input("Want to add another task? press 1 to continute , -1 to kill "))
-
-
-
-
+if navigation==2:
+    print("Tasks today: ")
+    store_txt("",2)
+if navigation==9:
+    store_txt("",9)
 
